01870e024fa27f632cc0376777e2dbb3cb508f82
Renegatto <46404781+Renegatto@users.noreply.github.com>
2021-02-11 03:07:07 +0300

    Update ban.js
    DRY

--- commands/ban.js
+++ commands/ban.js
@@ -1,4 +1,17 @@
-const naibPositionRole = require("../naibPositionRole.js")
+const higherRole = require("../naibPositionRole.js")
+
+const default_embed = (tag,avatar,color) => (title, descr) => {
+    embed: {
+        "title": title,
+        "description": descr,
+        "footer": {
+            "text": `Запросил ${tag}`,
+            "icon_url": avatar()
+        },
+        "color": color
+    }
+};
+
 module.exports = {
     name: "ban",
     use: "ban [user ping] [reason] - бан пользователя",
@@ -10,64 +23,25 @@ module.exports = {
     async run(client, msg, db, color, cmds) {
         msg.delete();
         let logs = msg.guild.channels.cache.get("801898794064150609");
-        let m = msg.content.split(" ")
-        m = m[1];
         let user = msg.mentions.members.first();
-        let isModer = null;
-        let pricina = msg.content.split(" ");
-        pricina.shift()
-        pricina.shift()
-        pricina = pricina.join(" ");
-        if (!user) {
-            msg.channel.send({
-                embed: {
-                    "title": `БАН`,
-                    "description": "Пользователь не найден",
-                    "footer": {
-                        "text": `Запросил ${msg.author.tag}`,
-                        "icon_url": msg.author.avatarURL()
-                    },
-                    "color": color
-                }
-            })
+        let reason = msg.content.split(" ").shift().shift().join(" ") || "Не указана";
+        
+        let embed = default_embed(msg.author.tag, msg.author.avatarURL, color)
+        
+        if (!user) {msg.channel.send(embed(`БАН`,"Пользователь не найден"))
         } else {
-            if (naibPositionRole(msg.member).position > naibPositionRole(user).position) {
-                logs.send({
-                    embed: {
-                        "title": `Команда [БАН]`,
-                        "description": "Пользователь " + user.user.tag + " был забанен модератором " + msg.author.tag + "\n По причине: " + (pricina || "Не указана"),
-                        "footer": {
-                            "text": `Запросил ${msg.author.tag}`,
-                            "icon_url": msg.author.avatarURL()
-                        },
-                        "color": color
-                    }
-                });
-                user.send({
-                    embed: {
-                        "title": `БАН`,
-                        "description": "Вы были забанены на сервере **Eternal Solitude** модератором " + msg.author.tag + "\n По причине: " + (pricina || "Не указана"),
-                        "footer": {
-                            "text": `Запросил ${msg.author.tag}`,
-                            "icon_url": msg.author.avatarURL()
-                        },
-                        "color": color
-                    }
-                }).then(x => {
-                    user.ban();
-                });
+            if (higherRole(msg.member).position > higherRole(user).position) {
+                logs.send(embed(
+                    `Команда [БАН]`,
+                    `Пользователь ${user.user.tag} был забанен модератором ${msg.author.tag}\n По причине: ${reason}`
+                ))
+                user.send(embed(
+                    `БАН`,
+                    `Вы были забанены на сервере **Eternal Solitude** модератором ${msg.author.tag}\n По причине: ${reason}`
+                )).then(x => {user.ban()})
+                          
             } else {
-                msg.channel.send({
-                    embed: {
-                        "title": `БАН`,
-                        "description": "Вы не можете забанить данного пользователя",
-                        "footer": {
-                            "text": `Запросил ${msg.author.tag}`,
-                            "icon_url": msg.author.avatarURL()
-                        },
-                        "color": color
-                    }
-                });
+                msg.channel.send(embed(`БАН`,"Вы не можете забанить данного пользователя"))
             }
         }
     }
